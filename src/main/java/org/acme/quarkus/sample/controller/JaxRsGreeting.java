87ffb216d8937bc48bda430dbcf2446a762d0b08
Hendi Santika <hendisantika@yahoo.co.id>
2021-10-26 15:37:52 +0700

    #22 - Add newHelloJaxRs in JaxRsGreeting

--- src/main/java/org/acme/quarkus/sample/controller/JaxRsGreeting.java
+++ src/main/java/org/acme/quarkus/sample/controller/JaxRsGreeting.java
@@ -5,6 +5,7 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -28,5 +29,11 @@ public class JaxRsGreeting {
     @Path("/hello")
     public Greeting helloJaxRs() {
         return new Greeting("Hello", "JAX-RS");
+    }
+
+    @POST
+    @Path("/hello")
+    public Greeting newHelloJaxRs(Greeting greeting) {
+        return greeting;
     }
 }
