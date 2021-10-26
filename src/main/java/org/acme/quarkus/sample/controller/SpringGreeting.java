1d801ee41d77def75c5c5b563c63d4b6665b3fbb
Hendi Santika <hendisantika@yahoo.co.id>
2021-10-26 15:41:19 +0700

    #30 - Add newHelloSpring & deleteHelloSpring in SpringGreeting

--- src/main/java/org/acme/quarkus/sample/controller/SpringGreeting.java
+++ src/main/java/org/acme/quarkus/sample/controller/SpringGreeting.java
@@ -3,7 +3,11 @@ package org.acme.quarkus.sample.controller;
 import org.acme.quarkus.sample.model.Greeting;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +28,15 @@ public class SpringGreeting {
     @GetMapping("/hello")
     public Greeting helloSpring() {
         return new Greeting("Hello", "Spring");
+    }
+
+    @PostMapping("/hello")
+    public Greeting newHelloSpring(@RequestBody Greeting greeting) {
+        return greeting;
+    }
+
+    @DeleteMapping("/hello/{message}")
+    public void deleteHelloSpring(@PathVariable(name = "message") String message) {
+
     }
 }
