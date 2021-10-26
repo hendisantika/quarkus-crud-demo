670352b0e02821df267350de7873de6776bc4f51
Hendi Santika <hendisantika@yahoo.co.id>
2021-10-26 15:37:17 +0700

    #20 - Add JaxRsGreeting

---
+++ src/main/java/org/acme/quarkus/sample/controller/JaxRsGreeting.java
@@ -0,0 +1,25 @@
+package org.acme.quarkus.sample.controller;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-crud-demo
+ * User: TOSHIBA
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/26/2021
+ * Time: 3:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Path("/jax-rs")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "JAX-RS Resource", description = "Basic Hello World using JAX-RS")
+public class JaxRsGreeting {
+}
