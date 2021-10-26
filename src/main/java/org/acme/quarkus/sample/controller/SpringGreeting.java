b7ab65b3add866524d5ec788a0870759d31fad9c
Hendi Santika <hendisantika@yahoo.co.id>
2021-10-26 15:40:28 +0700

    #28 - Add SpringGreeting

---
+++ src/main/java/org/acme/quarkus/sample/controller/SpringGreeting.java
@@ -0,0 +1,23 @@
+package org.acme.quarkus.sample.controller;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-crud-demo
+ * User: TOSHIBA
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/26/2021
+ * Time: 3:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping(value = "/spring", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Spring Resource", description = "Basic Hello World using Spring")
+public class SpringGreeting {
+
+}
