8017c7ab0dd8581d18e265092e0197f8f47bdb5b
Hendi Santika <hendisantika@yahoo.co.id>
2021-10-26 15:39:21 +0700

    #25 - Add doGet in ServletGreeting

--- src/main/java/org/acme/quarkus/sample/controller/ServletGreeting.java
+++ src/main/java/org/acme/quarkus/sample/controller/ServletGreeting.java
@@ -1,9 +1,16 @@
 package org.acme.quarkus.sample.controller;
 
+import org.acme.quarkus.sample.model.Greeting;
+
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +26,13 @@ import javax.servlet.http.HttpServlet;
 public class ServletGreeting extends HttpServlet {
 
     private static final Jsonb JSONB = JsonbBuilder.create();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        response.setContentType("application/json");
+        Greeting greeting = new Greeting("Hello", "Other");
+        PrintWriter out = response.getWriter();
+        out.print(JSONB.toJson(greeting));
+    }
 }
