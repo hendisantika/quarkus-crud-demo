b8619045920cf37f3668ea621e1e10e0bab602d9
Hendi Santika <hendisantika@yahoo.co.id>
2021-10-26 15:43:31 +0700

    #31 - Add GreetingTest

---
+++ src/test/java/org/acme/quarkus/sample/model/GreetingTest.java
@@ -0,0 +1,19 @@
+package org.acme.quarkus.sample.model;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-crud-demo
+ * User: TOSHIBA
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/26/2021
+ * Time: 3:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@QuarkusTest
+public class GreetingTest {
+
+
+}
